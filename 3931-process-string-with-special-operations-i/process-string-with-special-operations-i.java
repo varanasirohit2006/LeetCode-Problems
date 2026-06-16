39c5346bb115586c4a10ed69598308de9b555a7c
Varanasi Rohit <varanasirohit2006@gmail.com>
2026-06-16 09:39:20 +0530

    Time: 110 ms (6.62%) | Memory: 96.2 MB (5.88%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -2,22 +2,30 @@ class Solution {
     public String processStr(String s) {
         ArrayList<Character> res = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '%') {
+            char ch = s.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                res.add(ch);
+                continue;
+            }
+
+            if (ch == '%') {
                 if (res.size() > 0) {
                     Collections.reverse(res);
                 }
-            } else if (s.charAt(i) == '#') {
+                continue;
+            } 
+            if (ch == '#') {
                 int ArraySize = res.size();
                 for (int j = 0; j < ArraySize; j++) {
                     res.add(res.get(j));
                 }
-            } else if (s.charAt(i) == '*') {
+                continue;
+            } 
+             if (ch == '*') {
                 if (res.size() >= 1) {
                     res.remove(res.size() - 1);
                 }
-            } else {
-                res.add(s.charAt(i));
-            }
+            } 
         }
          StringBuilder sb = new StringBuilder();
         for(int i=0;i<res.size();i++){
